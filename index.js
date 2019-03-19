5636b6145415a8c0c7177429b94b6f4c77512280
yqsf2018 <yqssf2018@gmail.com>
2019-03-19 12:58:08 -0700

    set up project

---
+++ index.js
@@ -0,0 +1,44 @@
+let dhCore = require('./lib/dh-core');
+let ptzConv = require('./lib/ptz-conv');
+
+exports.init = function(camCfg, cb){
+
+};
+
+exports.load = function(camCfgFile, cb){
+
+};
+
+exports.save = function(camID, camCfgFile, cb){
+
+};
+
+exports.getCamCfg = function(camID, cb){
+
+};
+
+exports.getPTZ = function(camID, cb){
+
+};
+
+exports.resetPTZ = function(camID, cb){
+
+};
+
+exports.setPTZ = function(camID, ptzCfg,  cb){
+
+};
+
+exports.gotoRef = function(camID, refID, cb){
+
+};
+
+exports.setRef = function(camID, refID, cb){
+
+};
+
+exports.gotoXY = function(camID, x, y, cb){
+
+};
+
+
